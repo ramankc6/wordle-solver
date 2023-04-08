402486fe9379e37bc309f49546de5e33e383232f
ramankc6 <ramankc699@gmail.com>
2023-04-08 11:10:34 -0400

    added start and end time calc to test

--- Old Algorithm/test.py
+++ Old Algorithm/test.py
@@ -1,4 +1,5 @@
 from solver import *
+import time
 
 def test(word_list, bank_size, num_tests, algorithm):
     word_list = set(random.sample(word_list, bank_size))
@@ -7,6 +8,7 @@ def test(word_list, bank_size, num_tests, algorithm):
     beat_wordle = 0
     total_attempts = 0
     counter = 1
+    start_time = time.time()
     for answer in answers:
         print(f"Test #{counter} Answer: {answer}")
         if algorithm == 'random':
@@ -15,10 +17,12 @@ def test(word_list, bank_size, num_tests, algorithm):
             attempts = most_common_solver(answer, word_list)
         elif algorithm == 'best_guess':
             attempts = best_guess_solver(answer, word_list)
-        if attempts <= 5:
+        if attempts <= 6:
             beat_wordle += 1
         total_attempts += attempts
         counter += 1
+    end_time = time.time()
+    print(f'Time taken: {end_time - start_time} seconds')
     print(f'Chances of Beating Wordle: {(beat_wordle / (counter - 1)) * 100}%')
     average_attempts = total_attempts / len(answers)
     print(f'Average number of attempts: {average_attempts}')
